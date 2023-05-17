8039a9643dab4cdbcf3ab732e86e2f02725fb5d5
Nello Casolla <nellocasolla446@gmail.com>
2023-05-17 15:35:07 +0200

    assets

--- src/main/java/esercizio1/Location.java
+++ src/main/java/esercizio1/Location.java
@@ -1,5 +1,44 @@
 package esercizio1;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "gestioneventi")
+@Getter
+@Setter
+@ToString
+
 public class Location {
 
+	@Id
+	@GeneratedValue // Obbligatorio usarlo se si vuol fare gestire gli id al db
+	// private UUID id;
+	private long id;
+	private String titolo;
+	private LocalDate dataEvento;
+	private String descrizione;
+	private String tipoEvento; // [pubblico, privato]
+	private int numeroMassimoPartecipanti;
+
+	public Location() {
+	}
+
+	public Location(String titolo, LocalDate dataEvento, String descrizione, String tipoEvento,
+			int numeroMassimoPartecipanti) {
+		super();
+		this.titolo = titolo;
+		this.dataEvento = dataEvento;
+		this.descrizione = descrizione;
+		this.tipoEvento = tipoEvento;
+		this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+	}
 }
--- src/main/java/esercizio1/Main1.java
+++ src/main/java/esercizio1/Main1.java
@@ -1,10 +1,38 @@
 package esercizio1;
 
+import java.time.LocalDate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import dao.EventoDAO;
+import lombok.extern.slf4j.Slf4j;
+import utils.JpaUtil;
+
+@Slf4j
 public class Main1 {
+	private static EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EntityManager em = emf.createEntityManager();
+		EventoDAO ed = new EventoDAO(em);
 
+		// ************************ SAVE *********************
+		Evento one = new Evento("Buon Compleanno", LocalDate.now(), "18esimo compleanno", "disco", 50);
+		ed.save(one);
+
+		// ************************ FIND BY ID *********************
+
+		System.out.println(ed.getById(0));
+
+		// ************************ DELETE *********************
+		ed.delete(0);
+
+		// ************************ REFRESH ********************
+		ed.refresh(7);
+		em.close();
+
+		emf.close();
 	}
 
 }
